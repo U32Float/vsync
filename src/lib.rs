b69cad212b22801425ed9e396457b1ecd684e291
U32Float <123883732+U32Float@users.noreply.github.com>
2023-12-24 22:15:44 +0100

    predict next frame

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::{
-    sync::{Arc, Mutex, OnceLock},
-    time::Duration,
+    sync::{Arc, OnceLock, RwLock},
+    time::{Duration, SystemTime},
 };
 
 use dashmap::DashMap;
@@ -9,59 +9,90 @@ use dashmap::DashMap;
 
 pub type DisplayID = u32;
 
-pub struct VSync {}
-
-impl VSync {
-    pub fn time_until_next_frame(display: DisplayID) -> Duration {
-        unimplemented!()
-    }
-
-    pub fn wait_until_next_frame(display: DisplayID) {
-        Self::display_link(display).wait_vsync();
-    }
+pub fn time_until_next_frame(display: DisplayID) -> Duration {
+    display_link(display).time_until_next_frame()
 }
 
-impl VSync {
-    fn display_link(display: DisplayID) -> DisplayLink {
-        static DISPLAY_LINKS: OnceLock<DashMap<DisplayID, DisplayLink>> = OnceLock::new();
+fn display_link(display: DisplayID) -> DisplayLink {
+    static DISPLAY_LINKS: OnceLock<DashMap<DisplayID, DisplayLink>> = OnceLock::new();
 
-        DISPLAY_LINKS
-            .get_or_init(|| DashMap::new())
-            .get(&display)
-            .map(|l| l.clone())
-            .or_else(|| {
-                let link = DisplayLink::new(display);
-                DISPLAY_LINKS.get().unwrap().insert(display, link.clone());
-                Some(link)
-            })
-            .unwrap()
-    }
+    DISPLAY_LINKS
+        .get_or_init(|| DashMap::new())
+        .get(&display)
+        .map(|l| l.clone())
+        .or_else(|| {
+            let link = DisplayLink::new(display);
+            DISPLAY_LINKS.get().unwrap().insert(display, link.clone());
+            Some(link)
+        })
+        .unwrap()
 }
 
 // ----------------------------------------------------------------------------
 
+struct State {
+    /// The time for the next frame in nanoseconds since UNIX_EPOCH.
+    next_frame: Option<u128>,
+    /// The time a single frame takes in nanoseconds.
+    frame_time: Option<u32>,
+}
+
 #[derive(Clone)]
 struct DisplayLink {
-    next_frame: Arc<Mutex<Option<Duration>>>,
+    state: Arc<RwLock<State>>,
     /// Only used for keeping the display link alive.
     _wrapper: Arc<DisplayLinkWrapper>,
 }
 
 impl DisplayLink {
-    pub fn new(display: DisplayID) -> Self {
-        let mut link = display_link::DisplayLink::on_display(display, |t| {
-            println!("{:?}", t);
+    fn new(display: DisplayID) -> Self {
+        let state = Arc::new(RwLock::new(State {
+            next_frame: None,
+            frame_time: None,
+        }));
+
+        let state_clone = state.clone();
+        let mut link = display_link::DisplayLink::on_display(display, move |t| {
+            let duration_since_epoch = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap();
+            let timestamp_nanos = duration_since_epoch.as_nanos();
+            let next_frame = timestamp_nanos + t.nanos_since_zero as u128;
+            let prev = state_clone.read().unwrap().next_frame.clone();
+            if let Some(prev) = prev {
+                state_clone.write().unwrap().frame_time = Some((next_frame - prev) as u32)
+            }
+            state_clone.write().unwrap().next_frame = Some(next_frame);
         })
         .unwrap();
         let _ = link.resume();
 
         Self {
-            next_frame: Arc::new(Mutex::new(None)),
+            state,
             _wrapper: Arc::new(DisplayLinkWrapper::new(link)),
         }
     }
 
-    pub fn wait_vsync(&self) {}
+    fn time_until_next_frame(&self) -> Duration {
+        let duration_since_epoch = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let timestamp_nanos = duration_since_epoch.as_nanos();
+
+        let next_frame = self.state.read().unwrap().next_frame.clone();
+        if let Some(next_frame) = next_frame {
+            if next_frame > timestamp_nanos {
+                return Duration::from_nanos((next_frame - timestamp_nanos) as u64);
+            } else {
+                if let Some(frame_time) = self.state.read().unwrap().frame_time.clone() {
+                    return Duration::from_nanos(
+                        (frame_time as i64 - (timestamp_nanos - next_frame) as i64).max(0) as u64,
+                    );
+                }
+            }
+        }
+        return Duration::from_nanos(0);
+    }
 }
 
 // ----------------------------------------------------------------------------
@@ -74,7 +105,7 @@ unsafe impl Send for DisplayLinkWrapper {}
 unsafe impl Sync for DisplayLinkWrapper {}
 
 impl DisplayLinkWrapper {
-    pub fn new(display_link: display_link::DisplayLink) -> Self {
+    fn new(display_link: display_link::DisplayLink) -> Self {
         Self {
             ptr: Box::into_raw(Box::new(display_link)),
         }
